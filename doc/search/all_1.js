311df7530e912dcd588fd3bb1b4b5580b3be0b75
Github Action <invalid@aardsoft.fi>
2024-07-31 21:03:31 +0000

    Update doxygen documentation

--- doc/search/all_1.js
+++ doc/search/all_1.js
@@ -1,16 +1,9 @@
 var searchData=
 [
-  ['wsl_2dlauncher_2ec_0',['wsl-launcher.c',['../wsl-launcher_8c.html',1,'']]],
-  ['wsl_2dlauncher_2eh_1',['wsl-launcher.h',['../wsl-launcher_8h.html',1,'']]],
-  ['wsl_2dlog_2ec_2',['wsl-log.c',['../wsl-log_8c.html',1,'']]],
-  ['wsl_2dlog_2eh_3',['wsl-log.h',['../wsl-log_8h.html',1,'']]],
-  ['wsl_2dservice_2ec_4',['wsl-service.c',['../wsl-service_8c.html',1,'']]],
-  ['wsl_5floglevels_5',['WSL_LOGLEVELS',['../wsl-log_8h.html#a2bc9348f2b8b368c89a4dd125944bbbb',1,'wsl-log.h']]],
-  ['wsllog_6',['wsllog',['../wsl-log_8c.html#a5d858dd0d1370d6736665bbd2d6fd8b8',1,'wslLog(int level, LPCWSTR logFormat,...):&#160;wsl-log.c'],['../wsl-log_8h.html#a5d858dd0d1370d6736665bbd2d6fd8b8',1,'wslLog(int level, LPCWSTR logFormat,...):&#160;wsl-log.c']]],
-  ['wslloginit_7',['wslLogInit',['../wsl-log_8c.html#acc33eb90cda26b784dc6d66c3a86af8a',1,'wsl-log.c']]],
-  ['wsllogtext_8',['wsllogtext',['../wsl-log_8c.html#a2cf43504fd0642c54900860fb78a8913',1,'wslLogText(int level, LPCWSTR logMessage):&#160;wsl-log.c'],['../wsl-log_8h.html#a2cf43504fd0642c54900860fb78a8913',1,'wslLogText(int level, LPCWSTR logMessage):&#160;wsl-log.c']]],
-  ['wslrestoreuid_9',['wslrestoreuid',['../wsl-launcher_8c.html#a65a0a6a8181f7cc5a02cab7b36279544',1,'wslRestoreUid():&#160;wsl-launcher.c'],['../wsl-launcher_8h.html#a65a0a6a8181f7cc5a02cab7b36279544',1,'wslRestoreUid():&#160;wsl-launcher.c']]],
-  ['wslservicethreadinteractive_10',['wslservicethreadinteractive',['../wsl-launcher_8c.html#ac56aaa15a25bf501d03d2ad309010eff',1,'wslServiceThreadInteractive(WCHAR *serviceName):&#160;wsl-launcher.c'],['../wsl-launcher_8h.html#ac56aaa15a25bf501d03d2ad309010eff',1,'wslServiceThreadInteractive(WCHAR *serviceName):&#160;wsl-launcher.c']]],
-  ['wslsetrootuid_11',['wslsetrootuid',['../wsl-launcher_8c.html#a9cec8bede1fed4efe30954377d2dccce',1,'wslSetRootUid():&#160;wsl-launcher.c'],['../wsl-launcher_8h.html#a9cec8bede1fed4efe30954377d2dccce',1,'wslSetRootUid():&#160;wsl-launcher.c']]],
-  ['wslsetuid_12',['wslsetuid',['../wsl-launcher_8c.html#ab2eb1c6af8189f5eb8157cf3241412af',1,'wslSetUid(ULONG uid, BOOL save):&#160;wsl-launcher.c'],['../wsl-launcher_8h.html#ab2eb1c6af8189f5eb8157cf3241412af',1,'wslSetUid(ULONG uid, BOOL save):&#160;wsl-launcher.c']]]
+  ['setwsllogfile_0',['setwsllogfile',['../wsl-log_8c.html#a6d9ceed9cd0446d2e695f93f4b0c820b',1,'setWslLogFile(LPCWSTR logFile):&#160;wsl-log.c'],['../wsl-log_8h.html#a6d9ceed9cd0446d2e695f93f4b0c820b',1,'setWslLogFile(LPCWSTR logFile):&#160;wsl-log.c']]],
+  ['setwslloginteractive_1',['setwslloginteractive',['../wsl-log_8c.html#a539c63d8d5bfe7fcbbaaa2464eea12a2',1,'setWslLogInteractive(BOOL logType):&#160;wsl-log.c'],['../wsl-log_8h.html#a539c63d8d5bfe7fcbbaaa2464eea12a2',1,'setWslLogInteractive(BOOL logType):&#160;wsl-log.c']]],
+  ['setwslloglevel_2',['setwslloglevel',['../wsl-log_8c.html#a01d72e1fa67e391c3486080417027733',1,'setWslLogLevel(int facility, int level):&#160;wsl-log.c'],['../wsl-log_8h.html#a01d72e1fa67e391c3486080417027733',1,'setWslLogLevel(int facility, int level):&#160;wsl-log.c']]],
+  ['startwslservice_3',['startWslService',['../wsl-launcher_8h.html#a69cb22bb475c9e279437b8a7397c50da',1,'wsl-launcher.h']]],
+  ['startwslserviceinteractive_4',['startwslserviceinteractive',['../wsl-launcher_8c.html#ad13d7c4b9e122d0040dc1c4347037636',1,'startWslServiceInteractive(WslInstance *instanceData):&#160;wsl-launcher.c'],['../wsl-launcher_8h.html#ad13d7c4b9e122d0040dc1c4347037636',1,'startWslServiceInteractive(WslInstance *instanceData):&#160;wsl-launcher.c']]],
+  ['startwslserviceinteractivea_5',['startwslserviceinteractivea',['../wsl-launcher_8c.html#ab252ecfad4c2036308d3df0d835fecfb',1,'startWslServiceInteractiveA(LPCSTR serviceName, LPCSTR distributionName, WslInstance *instanceData):&#160;wsl-launcher.c'],['../wsl-launcher_8h.html#ab252ecfad4c2036308d3df0d835fecfb',1,'startWslServiceInteractiveA(LPCSTR serviceName, LPCSTR distributionName, WslInstance *instanceData):&#160;wsl-launcher.c']]]
 ];
--- doc/search/searchdata.js
+++ doc/search/searchdata.js
@@ -1,24 +1,27 @@
 var indexSectionsWithContent =
 {
-  0: "sw",
-  1: "w",
-  2: "sw",
-  3: "w"
+  0: "_stw",
+  1: "_",
+  2: "w",
+  3: "stw",
+  4: "w"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files",
-  2: "functions",
-  3: "enums"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "enums"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files",
-  2: "Functions",
-  3: "Enumerations"
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Enumerations"
 };
 
